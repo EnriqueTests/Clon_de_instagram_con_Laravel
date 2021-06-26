b3b48bc5da508d38e2486571437d97777f707426
EnriqueTests <enriquenada@outlook.es>
2021-06-25 19:24:29 -0500

    Agregando username

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="" class="rounded-circle">
+            <img src="https://media-exp3.licdn.com/dms/image/C4E0BAQGLKj3JHcof0w/company-logo_200_200/0/1589990867649?e=2159024400&v=beta&t=V8puy6s_dYMSAsGHDbhTWfKdLkqoQD5NBhIv3kkmJMQ" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+            <div><h1></h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
@@ -16,6 +16,17 @@
             <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
             <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus animi, omnis assumenda, a harum asperiores aspernatur iusto possimus in repellendus sapiente aperiam, cum velit illum? Quae voluptate quo unde nostrum?</div>
             <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://i.ytimg.com/vi/UEgMll-cS7o/maxresdefault.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://i.ytimg.com/vi/UEgMll-cS7o/maxresdefault.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://i.ytimg.com/vi/UEgMll-cS7o/maxresdefault.jpg" class="w-100">
         </div>
     </div>
 </div>
