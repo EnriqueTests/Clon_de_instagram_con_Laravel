e909f1ecdf33682535238b6a02c5a198a51efd23
EnriqueTests <enriquenada@outlook.es>
2021-06-26 18:24:21 -0500

    Agregar relaciones Eloquent entre los modelos Profile y User

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,9 @@
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus animi, omnis assumenda, a harum asperiores aspernatur iusto possimus in repellendus sapiente aperiam, cum velit illum? Quae voluptate quo unde nostrum?</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url ?? 'N/A'}}</a></div>
         </div>
     </div>
     <div class="row pt-5">
