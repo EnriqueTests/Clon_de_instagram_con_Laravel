28c4cbbd40f46164c3102b60e23749101dba920a
EnriqueTests <enriquenada@outlook.es>
2021-06-28 13:30:08 -0500

    Restringiendo y autorizando acciones con Model Policy

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -12,6 +12,8 @@ class ProfilesController extends Controller
     }
 
     public function edit(User $user) {
+        $this->authorize('update', $user->profile);
+
         return view('profiles.edit', compact('user'));
     }
 
