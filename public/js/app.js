a5a966e2fdce8e1c2ed6d6531e1807821bd3849e
EnriqueTests <enriquenada@outlook.es>
2021-06-30 15:53:26 -0500

    Relacion muchos a muchos entre usuario y perfil

--- public/js/app.js
+++ public/js/app.js
@@ -1852,16 +1852,33 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  props: ['userId'],
+  data: function data() {
+    return {
+      status: this.follows
+    };
+  },
+  props: ['userId', 'follows'],
   methods: {
     followUser: function followUser() {
+      var _this = this;
+
       axios.post('/follow/' + this.userId).then(function (response) {
-        alert(response.data);
+        _this.status = !_this.status;
+        console.log(response.data);
+      })["catch"](function (errors) {
+        if (errors.response.status == 401) {
+          window.location = '/login';
+        }
       });
     }
   },
   mounted: function mounted() {
     console.log('Component mounted.');
+  },
+  computed: {
+    buttonText: function buttonText() {
+      return this.status ? 'Unfollow' : 'Follow';
+    }
   }
 });
 
@@ -37420,11 +37437,11 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _c(
-      "button",
-      { staticClass: "btn btn-primary ml-4", on: { click: _vm.followUser } },
-      [_vm._v("Follow Me Now")]
-    )
+    _c("button", {
+      staticClass: "btn btn-primary ml-4",
+      domProps: { textContent: _vm._s(_vm.buttonText) },
+      on: { click: _vm.followUser }
+    })
   ])
 }
 var staticRenderFns = []
--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,22 +1,39 @@
 <template>
     <div>
-        <button class="btn btn-primary ml-4" @click="followUser">Follow Me Now</button>
+        <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText"></button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['userId'],
+        data: function() {
+            return {
+                status: this.follows,
+            }
+        },
+        props: ['userId', 'follows'],
         methods: {
             followUser () {
                 axios.post('/follow/' + this.userId)
                     .then(response => {
-                        alert(response.data);
+                        this.status = ! this.status;
+
+                        console.log(response.data);
+                    })
+                    .catch(errors => {
+                        if(errors.response.status == 401) {
+                            window.location = '/login';
+                        }
                     });
             },
         },
         mounted() {
             console.log('Component mounted.')
+        },
+        computed: {
+            buttonText() {
+                return (this.status) ? 'Unfollow' : 'Follow';
+            }
         }
     }
 </script>
