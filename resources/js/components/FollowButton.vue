41eab500e80ab76123effdbdf00586c08d0b8cb6
EnriqueTests <enriquenada@outlook.es>
2021-06-30 13:53:19 -0500

    Boton de follow con Vue

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button class="btn btn-primary ml-4" @click="followUser">Follow Me Now</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['userId'],
+        methods: {
+            followUser () {
+                axios.post('/follow/' + this.userId)
+                    .then(response => {
+                        alert(response.data);
+                    });
+            },
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
